0e0bb2cba10e9ab307e6577a2d8218d72986412c
TomChen1501 <43362640+TomChen1501@users.noreply.github.com>
2021-05-04 06:49:47 +0800

    basic movement of the aliens
    also contains some adjustment of numbers of bullets, numbers of enemy and their speed etc.

--- settings.py
+++ settings.py
@@ -8,9 +8,15 @@ class Settings:
         self.bg_color = (230,230,230)
         self.ship_speed = 1.5
 
-        #bullet settings
+        # bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
+
+        # aliens settings
+        self.alien_speed = 0.7
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 means right, = -1 means left
+        self.fleet_direction = 1
