83bdbd9dde6c23909fd809bb184861bfeb2ec449
TomChen1501 <43362640+TomChen1501@users.noreply.github.com>
2021-05-05 16:54:51 +0800

    Create game_stats.py

---
+++ game_stats.py
@@ -0,0 +1,10 @@
+class GameStats:
+    # status for the game
+    def __init__(self, ai_game):
+        # initialise the statistics
+        self.settings = ai_game.settings
+        self.reset_stats()
+        self.game_active = False
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
